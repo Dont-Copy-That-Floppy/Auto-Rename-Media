595b79f8225bda5429a49a1934600f8b9ef146ed
jake <jake@circuitspecialists.com>
2020-04-22 22:26:37 -0700

    working: download import database connection db insertion

--- auto-rename-media.py
+++ auto-rename-media.py
@@ -1,14 +1,23 @@
 import os
 import requests
-import database_manager
 import gzip
 import sys
+import pymongo
 
 
 class RENAME:
     def __init__(self):
-        episodes = "Hot to the Touch,Five Short Graybles,Web Weirdos,Dream of Love,Return to the Nightosphere,Daddy's Little Monster,In Your Footsteps,Hug Wolf,Princess Monster Wife,Goliad,Beyond This Earthly Realm,Gotcha!,Princess Cookie,Card Wars,Sons of Mars,Burning Low,BMO Noire,King Worm,Lady & Peebles,You Made Me,Who Would Win,Ignition Point,The Hard Easy,Reign of Gunters,I Remember You"
-        episode_array = episodes.split(',')
+        retitle_dir = 'I:/Retitle/'
+        tv_shows = []
+        movies = []
+        for file in os.listdir(retitle_dir):
+            if(os.path.isfile(file)):
+                movies.append(file)
+            else:
+                tv_shows.append(file)
+
+        print(tv_shows)
+        season_url = 'https://www.imdb.com/title/tt0103359/episodes?season=1'
 
     def getFiles(self):
         self.files = os.listdir()
@@ -16,55 +25,111 @@ class RENAME:
 
 class DATASET:
     def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.imdb = self.client["imdb"]
+
         self.main_url = "https://datasets.imdbws.com/"
-        self.names_basics = "name.basics.tsv.gz"
-        self.title_akas = "title.akas.tsv.gz"
-        self.title_basics = "title.basics.tsv.gz"
-        self.title_crew = "title.crew.tsv.gz"
-        self.title_episodes = "title.episode.tsv.gz"
-        self.title_principles = "title.principals.tsv.gz"
-        self.title_ratings = "title.ratings.tsv.gz"
-        self.db = database_manager.MANAGER()
+        filenames = ["name.basics.tsv.gz", "title.akas.tsv.gz", "title.basics.tsv.gz",
+                     "title.crew.tsv.gz", "title.episode.tsv.gz", "title.principals.tsv.gz", "title.ratings.tsv.gz"]
+        self.getNewSet(filenames)
 
-    def getNewSet(self):
-        self.downloadSet(self.names_basics)
-        self.downloadSet(self.title_akas)
-        self.downloadSet(self.title_basics)
-        self.downloadSet(self.title_crew)
-        self.downloadSet(self.title_episodes)
-        self.downloadSet(self.title_principles)
-        self.downloadSet(self.title_ratings)
+        self.addTable(filenames, 5)
 
-    def downloadSet(self, set_type):
-        print('Checking ' + set_type)
-        response = requests.head(
-            self.main_url + set_type).headers['Content-length']
-        filename = set_type
-        file_size = os.path.getsize(filename)
-        if(int(response) != int(file_size)):
-            print('Getting ' + set_type)
-            response = requests.get(self.main_url + set_type)
-            file = open(filename, 'wb')
-            file.write(response.content)
-            file.close()
+        # for index in range(0, len(self.imdb_dict)):
+        #    self.setData(filenames[index], self.imdb_dict[index])
+
+    def addTable(self, filenames, index):
+        self.setData(filenames[index])
+
+    def setData(self, filename):
+        print('Setting Collection %s documents' % filename[:-len('.tsv.gz')])
+        collection_name = "%s" % filename[:-len('.tsv.gz')].replace('.', '-')
+        multiple_elements = False
+        with open(filename[:-len('.gz')], encoding='utf-8') as file:
+            line = file.readline().replace('\n', '')
+            header_elements = line.split('\t')
+            multiple_elements = (header_elements[1] == 'ordering')
+
+            while line != '':
+                line = file.readline().replace('\n', '')
+                elements = line.split('\t')
+                if(not multiple_elements):
+                    document = {'_id': '%s' % elements[0]}
+
+                    for index in range(1, len(header_elements)):
+                        try:
+                            document.update({'%s' % header_elements[index]: '%s' % elements[index]})
+                        except:
+                            pass
+                else:
+                    document_id = elements[0]
+
+                    document = {'_id': '%s' % elements[0]}
+                    while True:
+                        ordering_dict = {'%s' % elements[1]: {}}
+                        for index in range(2, len(header_elements)):
+                            try:
+                                ordering_dict['%s' % elements[1]].update({'%s' % header_elements[index]: '%s' % elements[index]})
+                            except:
+                                pass
+
+                        temp_position = file.tell()
+                        line = file.readline().replace('\n', '')
+                        elements = line.split('\t')
+                        current_document_id = elements[0]
+                        if(current_document_id == document_id):
+                            document.update(ordering_dict)
+                        else:
+                            document.update(ordering_dict)
+                            file.seek(temp_position)
+                            break
+
+                try:
+                    self.imdb[collection_name].insert_one(document)
+                except Exception as e:
+                    print(e)
+
+    def getNewSet(self, filenames):
+        for filename in filenames:
+            self.downloadSet(filename)
+
+    def downloadSet(self, filename):
+        if(not os.path.exists('./' + filename)):
+            print("%s does not exist.\nDownloading %s..." %
+                  (filename, filename))
+            self.download(filename)
+            self.decompress(filename)
+            self.setData(filename)
+        elif(self.downloadFileSize(filename) != os.path.getsize(filename)):
+            print('%s exists, and content size mismatch.' % filename)
+            self.download(filename)
+            self.decompress(filename)
+            self.setData(filename)
         else:
-            print('File size matches')
+            print('%s exists, and size matches' % filename)
 
-    def writeSet2DB(self):
-        self.db.addName('basics')
+    def downloadFileSize(self, filename):
+        response = requests.get(self.main_url + filename,
+                                stream=True).headers['Content-length']
+        return int(response)
 
-    def updateType(self, set_type):
-        print()
+    def download(self, filename):
+        response = requests.get(self.main_url + filename)
+        with open(filename, 'wb') as file:
+            file.write(response.content)
 
     def decompress(self, filename):
-        file = gzip.open(filename, 'rb')
-        self.decompressed_file = file.read()
-        file.close()
-        print(sys.getsizeof(self.decompressed_file))
+        with open(filename[:-len('.gz')], 'wb') as uFile:
+            with gzip.open(filename, 'rb') as file:
+                file_cache = file.read(1024)
+                uFile.write(file_cache)
+                while file_cache:
+                    file_cache = file.read(1024)
+                    uFile.write(file_cache)
+
+        print('Decompressed: %s' % filename)
 
 
 if __name__ == "__main__":
     # RENAME()
-    main = DATASET()
-    main.getNewSet()
-    main.decompress('imdb_' + main.title_ratings)
+    DATASET()
